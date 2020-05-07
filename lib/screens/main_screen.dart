8a1de48285870095bbcee112763d650627503be6
Jethro Lising <lising.jg@gmail.com>
2020-05-07 16:06:55 -0400

    Removed dead code

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,7 +5,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:just_breathe/constants/preset_timers.dart';
 import 'package:just_breathe/constants/theme.dart';
 import 'package:just_breathe/constants/ui.dart';
-import 'package:just_breathe/data/preset_timers.dart';
 import 'package:just_breathe/data/settings.dart';
 import 'package:just_breathe/generated/l10n.dart';
 import 'package:just_breathe/pages_routes.dart';
